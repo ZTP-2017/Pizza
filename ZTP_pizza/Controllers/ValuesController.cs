fb5ca9da3132a1b7321717e81170c3be975136b4
Michał Krzus <michalkrzus93@gmail.com>
2017-10-22 16:56:44 +0200

    change project api to web api

---
+++ ZTP_pizza/Controllers/ValuesController.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ZTP_pizza.Services;
+using MongoDB.Bson;
+using System.Collections.Generic;
+
+namespace ZTP_pizza.Controllers
+{
+    [Route("api/[controller]")]
+    public class ValuesController : Controller
+    {
+        private readonly IDataService dataService;
+
+        public ValuesController(IDataService dataService)
+        {
+            this.dataService = dataService;
+        }
+
+        // GET api/values
+        [HttpGet]
+        public string Get(string lang)
+        {
+            var result = new List<string>();
+
+            foreach (var pizza in dataService.GetAll())
+            {
+                var content = dataService.GetPizzaContentByLang(pizza, lang);
+                result.Add(content.ToJson());
+            }
+            
+            return result.ToJson();
+        }
+
+        // GET api/values/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/values
+        [HttpPost]
+        public void Post([FromBody]string value)
+        {
+        }
+
+        // PUT api/values/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE api/values/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
